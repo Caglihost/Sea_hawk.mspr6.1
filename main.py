9ffa8614f136e89840275169efbb386e82fbfa0d
Caglihost <160582058+Caglihost@users.noreply.github.com>
2025-04-29 19:32:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import shutil # Importation du module shutil pour les opérations de fichiers
 import sys  # Nécessaire pour lancer update.py et quitter l'application
 
 # Application version
-APP_VERSION = "1.2.10"
+APP_VERSION = "1.2.11"
 
 def check_for_update():
     url = "https://api.github.com/repos/Caglihost/Sea_hawk.mspr6.1/releases/latest"
